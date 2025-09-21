83772709d6633f08d924a8807104e5798a90cd83
Alexander Metzner <alexander.metzner@gmail.com>
2025-09-21 12:47:55 +0200

    feature: diceroller history sum and clear (#6)

--- src/diceroller/models/index.ts
+++ src/diceroller/models/index.ts
@@ -5,6 +5,18 @@ export class DieRoll {
     constructor(public readonly dieType: DieType, public readonly result: number) { }
 }
 
+export class AggregatedRoll {
+    constructor(public readonly rolls: Array<DieRoll>) {}
+
+    countOfDie (t: DieType): number {
+        return this.rolls.filter(r => r.dieType === t).length
+    }
+
+    get sum(): number {
+        return this.rolls.reduce((p, c) => p + c.result, 0)
+    }
+}
+
 export class Model {
     static createInitial(): Model {
         return new Model([3, 4, 6, 8, 10, 12, 20, 100])
@@ -14,6 +26,14 @@ export class Model {
 
     rollDie(die: DieType): Model {
         const roll = new DieRoll(die, Math.floor(Math.random() * die) + 1)
-        return new Model(this.availableDice, [roll].concat(this.history) , roll)
+        return new Model(this.availableDice, [roll].concat(this.history.slice(0, 9)) , roll)
+    }
+
+    clearHistory(): Model {
+        return new Model(this.availableDice)
+    }
+
+    get aggregatedHistory(): AggregatedRoll {
+        return new AggregatedRoll(this.history)
     }
 }
